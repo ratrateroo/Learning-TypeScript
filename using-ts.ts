2b9e9e9ce8fe7cc5f3688e1eaf399df15c0051aa
Mark <ratrateroo@gmail.com>
2020-02-26 17:08:58 +0800

    added type script file

--- using-ts.ts
+++ using-ts.ts
@@ -1,6 +1,6 @@
 var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
+var input1 = document.getElementById("num1")! as HTMLInputElement;
+var input2 = document.getElementById("num2")! as HTMLInputElement;
 function add(num1, num2) {
     return num1 + num2;
 }
