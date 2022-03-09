5485048c51f2db42f242c59c95668986b204a543
Mark <ratrateroo@gmail.com>
2022-03-09 22:59:50 +0800

    updated done and delete capability

--- learning-typescript/src/components/SingleTodo.tsx
+++ learning-typescript/src/components/SingleTodo.tsx
@@ -19,24 +19,35 @@ type Props = {
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
 	const [edit, setEdit] = useState<boolean>(false);
 	const [editTodo, setEditTodo] = useState<string>(todo.todo);
-	const todoEditHandler = (isDone: boolean) => {
-		if (!isDone && !edit) setEdit(!edit);
+	const todoEditHandler = (isDone: boolean, id: number) => {
+		!isDone && !edit
+			? setEdit(!edit)
+			: setTodos(
+					todos.map((todo) =>
+						todo.id === id ? { ...todo, todo: editTodo } : todo
+					)
+			  );
+		setEdit(!edit);
 	};
 	const todoDoneHandler = (id: number) => {
-		setTodos(
-			todos.map((todo) =>
-				todo.id === id
-					? {
-							...todo,
-							isDone: !todo.isDone,
-					  }
-					: todo
-			)
-		);
+		if (!edit) {
+			setTodos(
+				todos.map((todo) =>
+					todo.id === id
+						? {
+								...todo,
+								isDone: !todo.isDone,
+						  }
+						: todo
+				)
+			);
+		}
 	};
 
 	const todoDeleteHandler = (id: number) => {
-		setTodos(todos.filter((todo) => todo.id !== id));
+		if (!edit) {
+			setTodos(todos.filter((todo) => todo.id !== id));
+		}
 	};
 
 	const todoSubmitHandler = (e: React.KeyboardEvent, id: number) => {
@@ -59,7 +70,7 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
 						edge="end"
 						aria-label="edit"
 						onClick={() => {
-							todoEditHandler(todo.isDone);
+							todoEditHandler(todo.isDone, todo.id);
 						}}>
 						<EditOutlinedIcon />
 					</IconButton>
