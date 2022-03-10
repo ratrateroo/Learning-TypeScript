29b8ac6694215c3961617402b53c7038cbaac460
Mark <ratrateroo@gmail.com>
2022-03-10 17:44:29 +0800

    added interfaces and enum

--- learning-typescript/src/model.ts
+++ learning-typescript/src/model.ts
@@ -3,3 +3,18 @@ export interface Todo {
 	todo: string;
 	isDone: boolean;
 }
+
+export enum TodoActionKind {
+	EDIT = 'EDIT',
+	DELETE = 'DELETE',
+	DONE = 'DONE',
+}
+
+export interface TodoAction {
+	type: TodoActionKind;
+	payload: number;
+}
+
+export interface TodoState {
+	todos: Todo[];
+}
